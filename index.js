c5a4ff5d3b04b19bebe0fb8dab1612c4f48406d8
Cong Tuan <126613516+Tunnienguci@users.noreply.github.com>
2023-06-10 00:58:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
+/** @format */
+
 let endShow = document.getElementById("endShow");
 let startShow = document.getElementById("startShow");
 let main = document.querySelector(".formMain");
 let preview = document.querySelector(".preview__profile");
+let statusView = document.querySelector(".status");
 
-function previewProfile() {
-	if (preview.style.display == "none") {
-        preview.style.animation = "showPreview 0.5s ease-in-out";
-        main.style.animation = "openMain 0.5s ease-in-out"
-        preview.style.opacity = "1";
-        preview.style.zIndex = "1";
-        preview.style.transform = "translateX(0%)";
-        setTimeout(function () {
-            preview.style.display = "block";
-		}, 500);
-	} else {
-        preview.style.animation = "hidePreview 0.5s ease-in-out";
-        main.style.animation = "formMain 0.5s ease-in-out"
-        setTimeout(function () {
-            preview.style.display = "none";
-        }, 500);
-	}
-}
-
-endShow.addEventListener("click", previewProfile);
-startShow.addEventListener("click", previewProfile);
+endShow.addEventListener("click", function () {
+	preview.style.animation = "hidePreview 0.5s ease-in-out";
+	main.style.animation = "formMain 0.5s ease-in-out";
+	statusView.style.animation = "closeStatus 0.5s ease-in-out";
+	setTimeout(function () {
+		preview.style.display = "none";
+		preview.style.opacity = "0";
+	}, 500);
+});
+startShow.addEventListener("click", function () {
+	statusView.style.animation = "openStatus 0.5s ease-in-out";
+	preview.style.animation = "showPreview 0.5s ease-in-out";
+	main.style.animation = "openMain 0.5s ease-in-out";
+	preview.style.opacity = "1";
+	preview.style.zIndex = "1";
+	preview.style.transform = "translateX(0%)";
+	setTimeout(function () {
+		preview.style.display = "block";
+	}, 500);
+});
