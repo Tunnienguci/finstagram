b58858a477f26fc76a1a23c99cbdfcc6c1daa090
Cong Tuan <126613516+Tunnienguci@users.noreply.github.com>
2023-06-10 13:56:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let startShow = document.getElementById("startShow");
 let main = document.querySelector(".formMain");
 let preview = document.querySelector(".preview__profile");
 let statusView = document.querySelector(".status");
+let startShow2 = document.querySelector(".account__users");
 
 endShow.addEventListener("click", function () {
 	preview.style.animation = "hidePreview 0.5s ease-in-out";
@@ -25,4 +26,27 @@ startShow.addEventListener("click", function () {
 	setTimeout(function () {
 		preview.style.display = "block";
 	}, 500);
+});
+
+startShow2.addEventListener("click", function () {
+	statusView.style.animation = "openStatus 0.5s ease-in-out";
+	preview.style.animation = "showPreview 0.5s ease-in-out";
+	main.style.animation = "openMain 0.5s ease-in-out";
+	preview.style.opacity = "1";
+	preview.style.zIndex = "1";
+	preview.style.transform = "translateX(0%)";
+	setTimeout(function () {
+		preview.style.display = "block";
+	}, 500);
+});
+
+
+let lovebtn = document.querySelector(".fa-heart");
+lovebtn.addEventListener("click", function () {
+	if (lovebtn.style.color === "red") {
+		lovebtn.style.color = "black";
+	}
+	else {
+		lovebtn.style.color = "red";
+	}
 });
